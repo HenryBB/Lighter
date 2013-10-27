c61ff20f8ad327c7d5df1993763ead82776aac3a
HenryBB <Burmeister-BrownH@catlin.edu>
2013-10-26 22:48:28 -0700

    made filling work "better"

--- src/Runner.java
+++ src/Runner.java
@@ -29,7 +29,10 @@ public class Runner extends BasicGame {
 
 	// VARIABLES
 	ArrayList<Obstructable> obs = new ArrayList<Obstructable>();
-	Point2D[] windowPoints = {new Point2D.Float(0,0),new Point2D.Float(0,windowHeight),new Point2D.Float(windowWidth,windowHeight),new Point2D.Float(windowWidth,0)};
+	Point2D[] windowPoints = { new Point2D.Float(0, 0),
+			new Point2D.Float(0, windowHeight),
+			new Point2D.Float(windowWidth, windowHeight),
+			new Point2D.Float(windowWidth, 0) };
 	Obstructable windowObs = new Obstructable(windowPoints);
 	ArrayList<Light> lights = new ArrayList<Light>();
 
@@ -48,63 +51,87 @@ public class Runner extends BasicGame {
 		g.fillRect(0, 0, windowWidth, windowHeight);
 		for (Light l : lights) {
 			for (Point2D p : windowObs.getVertices()) {
-				Ray windowRay = new Ray(l.getLoc(),p);
+				Ray windowRay = new Ray(l.getLoc(), p);
 				windowRay.intersection = p;
+				windowRay.obs = windowObs;
 				l.addRay(windowRay);
 			}
 			for (Obstructable o : obs) {
 				Polygon ObsPoly = new Polygon();
 				for (Point2D p : o.getVertices()) {
 					Ray r = new Ray(l.getLoc(), p);
+					r.obs = o;
 					l.addRay(r);
-					ObsPoly.addPoint(cXout((float) p.getX()), cYout((float) p.getY()));
+					ObsPoly.addPoint(cXout((float) p.getX()),
+							cYout((float) p.getY()));
 				}
 				for (Ray r : l.rays) {
 					Point2D intersection = o.rayIntersection1(r);
 					if (intersection != null) {
 						if (r.intersection == null) {
 							r.intersection = intersection;
-						}
-						else if (r.origin.distanceSq(intersection) < r.origin.distanceSq(r.intersection)) {
+						} else if (r.origin.distanceSq(intersection) < r.origin
+								.distanceSq(r.intersection)) {
 							r.intersection = intersection;
 						}
 					}
 				}
-				g.setColor(Color.green);
+				g.setColor(Color.black);
 				g.fill(ObsPoly);
 			}
 			if (onScreen(l.location)) {
 				for (Ray r : l.rays) {
-					if (r.intersection==null) {
+					if (r.intersection == null) {
 						r.intersection = windowObs.rayIntersection1(r);
 					}
 				}
 			}
 			l.sortRays();
-			for (int i = 0; i < l.rays.size(); i++) {
-				
+			for (int i=0;i<l.rays.size();i++)
+			{
 				Ray r = l.ray(i);
-				
-				g.setColor(Color.blue);
-				g.drawLine(cXout((float)r.origin.getX()), cYout((float)r.origin.getY()), cXout((float)r.intersection.getX()), cYout((float)r.intersection.getY()));
-				
+				if (r.origin.distanceSq(r.intersection)<r.origin.distanceSq(r.tip))
+				{
+					l.rays.remove(r);
+					i--;
+				}
+			}
+			for (int i = 0; i < l.rays.size(); i++) {
+
+				Ray r = l.ray(i);
+
+//				g.setColor(Color.blue);
+//				g.drawLine(cXout((float) r.origin.getX()),
+//						cYout((float) r.origin.getY()),
+//						cXout((float) r.intersection.getX()),
+//						cYout((float) r.intersection.getY()));
+
 				Polygon poly = new Polygon();
-				
-				poly.addPoint(cXout((float)r.intersection.getX()),cYout((float)r.intersection.getY()));
-				poly.addPoint(cXout((float)r.origin.getX()),cYout((float)r.origin.getY()));
 				Ray r2;
 				if (i + 1 >= l.rays.size()) {
 					r2 = l.ray(0);
+				} else {
+					r2 = l.ray(i + 1);
 				}
-				else {
-					r2 = l.ray(i+1);
+				if (r.obs == r2.obs) {
+					poly.addPoint(cXout((float) r.tip.getX()),
+							cYout((float) r.tip.getY()));
+					poly.addPoint(cXout((float) r.origin.getX()),
+							cYout((float) r.origin.getY()));
+					poly.addPoint(cXout((float) r2.tip.getX()),
+							cYout((float) r2.tip.getY()));
+				} else {
+					poly.addPoint(cXout((float) r.intersection.getX()),
+							cYout((float) r.intersection.getY()));
+					poly.addPoint(cXout((float) r.origin.getX()),
+							cYout((float) r.origin.getY()));
+					poly.addPoint(cXout((float) r2.intersection.getX()),
+							cYout((float) r2.intersection.getY()));
 				}
-				poly.addPoint(cXout((float)r2.intersection.getX()),cYout((float)r2.intersection.getY()));
+
 				g.setColor(Color.white);
 				g.fill(poly);
-				g.setColor(Color.red);
-				g.drawString(i+"", cXin((float)r.tip.getX())+20, cYin((float)r.tip.getY()+20));
-				
+
 			}
 			l.clearRays();
 		}
@@ -113,14 +140,19 @@ public class Runner extends BasicGame {
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
-		lights.add(new Light(new Point2D.Float(windowWidth/2,windowHeight/2)));
-		Point2D[] testPoints = {new Point2D.Float(0,0),new Point2D.Float(0,windowHeight),new Point2D.Float(windowWidth,windowHeight),new Point2D.Float(windowWidth,0)};
+		lights.add(new Light(new Point2D.Float(windowWidth / 2,
+				windowHeight / 2)));
+		Point2D[] testPoints = { new Point2D.Float(0, 0),
+				new Point2D.Float(0, windowHeight),
+				new Point2D.Float(windowWidth, windowHeight),
+				new Point2D.Float(windowWidth, 0) };
 		Obstructable testObs = new Obstructable(testPoints);
-		Ray testRay = new Ray(new Point2D.Float(50,50),new Point2D.Float(51,60));
+		Ray testRay = new Ray(new Point2D.Float(50, 50), new Point2D.Float(51,
+				60));
 		Point2D intersection = testObs.rayIntersection1(testRay);
 		System.out.println("=================");
 		System.out.println(intersection);
-		
+
 	}
 
 	@Override
@@ -128,11 +160,11 @@ public class Runner extends BasicGame {
 		Input in = gc.getInput();
 		float x = cXin(in.getMouseX());
 		float y = cYin(in.getMouseY());
-		lights.get(0).setLocation(new Point2D.Float(x,y));
+		lights.get(0).setLocation(new Point2D.Float(x, y));
 	}
 
-	
 	ArrayList<Point2D> pressedPoints = new ArrayList<Point2D>();
+
 	@Override
 	public void mousePressed(int button, int x, int y) {
 		if (button == 0) {
@@ -144,7 +176,7 @@ public class Runner extends BasicGame {
 			obs.add(o);
 			pressedPoints.clear();
 		}
-		if (button==2) {
+		if (button == 2) {
 			pressedPoints.clear();
 			obs.clear();
 		}
@@ -155,33 +187,32 @@ public class Runner extends BasicGame {
 	public void mouseReleased(int button, int x, int y) {
 
 	}
-	
-	public float cYin(float y)
-	{
-		return windowHeight-y;
+
+	public float cYin(float y) {
+		return windowHeight - y;
 	}
-	
-	public float cYout(float y)
-	{
-		return windowHeight-y;
+
+	public float cYout(float y) {
+		return windowHeight - y;
 	}
-	public float cXin(float x)
-	{
-		return windowWidth-x;
+
+	public float cXin(float x) {
+		return windowWidth - x;
 	}
-	public float cXout(float x)
-	{
-		return windowWidth-x;
+
+	public float cXout(float x) {
+		return windowWidth - x;
 	}
+
 	public boolean onScreen(Point2D p) {
-		float x = cXout((float)p.getX());
-		float y = cYout((float)p.getY());
-		if ((x>0)&&(x<windowWidth)) {
-			if ((y>0)&&(y<windowHeight)) {
+		float x = cXout((float) p.getX());
+		float y = cYout((float) p.getY());
+		if ((x > 0) && (x < windowWidth)) {
+			if ((y > 0) && (y < windowHeight)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 }
